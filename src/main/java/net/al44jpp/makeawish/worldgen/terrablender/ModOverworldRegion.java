6adb0543256c28eadab2ea9423a342fb3aefb2d2
al44jpp <alexisclavreulpetit@gmail.com>
2025-08-24 11:20:36 +0200

    small change to rarity of the starwood forest

--- src/main/java/net/al44jpp/makeawish/worldgen/terrablender/ModOverworldRegion.java
+++ src/main/java/net/al44jpp/makeawish/worldgen/terrablender/ModOverworldRegion.java
@@ -8,8 +8,10 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.biome.Climate;
+import terrablender.api.ParameterUtils;
 import terrablender.api.Region;
 import terrablender.api.RegionType;
+import terrablender.api.VanillaParameterOverlayBuilder;
 
 import java.util.function.Consumer;
 
@@ -23,17 +25,20 @@ public class ModOverworldRegion extends Region
     @Override
     public void addBiomes(Registry<Biome> registry, Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> mapper) {
         // Insert Starwood Forest into forest-like climate, with low weight (weight is set in the Region ctor)
-        this.addBiome(mapper,
-                Climate.parameters(
-                        Climate.Parameter.point(0.25F), // temperature
-                        Climate.Parameter.point(0.8F),  // humidity
-                        Climate.Parameter.point(0.0F),  // continentalness (near inland)
-                        Climate.Parameter.point(0.0F),  // erosion
-                        Climate.Parameter.point(0.0F),  // depth
-                        Climate.Parameter.point(0.0F),  // weirdness
-                        0.0F // offset
-                ),
-                ModBiomes.STARWOOD_FOREST
-        );
+        VanillaParameterOverlayBuilder builder = new VanillaParameterOverlayBuilder();
+        // Overlap Vanilla's parameters with our own for our COLD_BLUE biome.
+        // The parameters for this biome are chosen arbitrarily.
+        new ParameterUtils.ParameterPointListBuilder()
+                .temperature(ParameterUtils.Temperature.span(ParameterUtils.Temperature.COOL, ParameterUtils.Temperature.FROZEN))
+                .humidity(ParameterUtils.Humidity.span(ParameterUtils.Humidity.NEUTRAL, ParameterUtils.Humidity.HUMID))
+                .continentalness(ParameterUtils.Continentalness.FAR_INLAND)
+                .erosion(ParameterUtils.Erosion.EROSION_0, ParameterUtils.Erosion.EROSION_1)
+                .depth(ParameterUtils.Depth.SURFACE, ParameterUtils.Depth.FLOOR, ParameterUtils.Depth.FULL_RANGE)
+                .weirdness(ParameterUtils.Weirdness.MID_SLICE_NORMAL_ASCENDING, ParameterUtils.Weirdness.MID_SLICE_NORMAL_DESCENDING)
+                .build().forEach(point -> builder.add(point, ModBiomes.STARWOOD_FOREST));
+
+        // Add our points to the mapper
+        builder.build().forEach(mapper);
+
     }
 }
